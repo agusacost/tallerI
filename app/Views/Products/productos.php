6b93e34e9eaaa964c76a4b305491bb16e2a418f6
agusacost <juanagustinacost@gmail.com>
2024-06-06 10:13:05 -0300

    updates

--- app/Views/Products/productos.php
+++
@@ -1,9 +0,0 @@
-<main>
-    <section class="container">
-        <div class="product-container">
-            <h1>Página en construcción</h1>
-            <h2> Gracias por aguardar</h2>
-            <img src="assets\img\construccion.png" alt="construccion">
-        </div>
-    </section>
-</main>
