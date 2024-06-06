afe1f0cdfc4df3eb7b021d0cc806a01902f041a8
agusacost <juanagustinacost@gmail.com>
2024-06-06 08:58:36 -0300

    auth user filter added

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -3,6 +3,7 @@
 namespace Config;
 
 use App\Filters\AuthFilter;
+use App\Filters\AuthUserFilter;
 use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
@@ -23,6 +24,7 @@ class Filters extends BaseConfig
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
         'authAdmin' => AuthFilter::class,
+        'authUser' => AuthUserFilter::class,
     ];
 
     /**
