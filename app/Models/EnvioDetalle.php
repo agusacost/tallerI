11822c874c4fd9c251dcc1a74b7bf70d89a36524
agusacost <juanagustinacost@gmail.com>
2024-06-07 18:38:19 -0300

    models added

--- app/Models/EnvioDetalle.php
+++ app/Models/EnvioDetalle.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class envioDetalle extends Model
+class EnvioDetalle extends Model
 {
     protected $table      = 'Envio_detalle';
     protected $primaryKey = 'id_envio';
--- app/Models/Products.php
+++ app/Models/Products.php
@@ -9,4 +9,16 @@ class Products extends Model
     protected $table      = 'product';
     protected $primaryKey = 'id';
     protected $allowedFields = ['nombre', 'descripcion', 'imagen', 'id_categoria', 'cantidad', 'precio'];
+
+    public function getCantidad($id)
+    {
+        $producto = $this->find($id);
+        return $producto['cantidad'];
+    }
+
+    public function updateCantidad($id, $nueva_cantidad)
+    {
+        $data = ['cantidad' => $nueva_cantidad];
+        $this->update($id, $data);
+    }
 }
