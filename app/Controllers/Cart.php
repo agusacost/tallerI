526d969d97679c28167802c0c7453c70fb2a3c3e
agusacost <juanagustinacost@gmail.com>
2024-06-07 18:38:00 -0300

    updates

--- app/Controllers/Cart.php
+++ app/Controllers/Cart.php
@@ -2,7 +2,10 @@
 
 namespace App\Controllers;
 
+use App\Models\EnvioDetalle;
 use App\Models\Products;
+use App\Models\VentasCabecera;
+use App\Models\VentasDetalle;
 
 class Cart extends BaseController
 {
@@ -58,6 +61,7 @@ class Cart extends BaseController
 
         return redirect()->to(base_url('productos'));
     }
+
     public function update()
     {
         $request = \Config\Services::request();
@@ -77,6 +81,7 @@ class Cart extends BaseController
 
         return redirect()->back()->withInput();
     }
+
     public function remove($rowid)
     {
         if ($rowid === "all") {
@@ -86,5 +91,100 @@ class Cart extends BaseController
         }
 
         return redirect()->back()->withInput();
+    }
+
+    public function proceder()
+    {
+        $cart = \Config\Services::cart();
+        $carrito = $this->cart->contents();
+
+        if (empty($carrito)) {
+            return redirect()->back()->with('mensaje', 'El carrito esta vacio');
+        }
+
+        echo view('components/header');
+        echo view('Products/comprar', ['productos' => $carrito, 'cart' => $cart]);
+        echo view('components/footer');
+    }
+
+    public function comprar()
+    {
+        $productoModelo = new Products();
+        $session = session();
+        $cart = \Config\Services::cart();
+
+        $carrito = $this->cart->contents();
+        if (empty($carrito)) {
+            return redirect()->back()->with('mensaje', 'El carrito esta vacio');
+        }
+
+        $usuario_id = $session->get('id');
+
+        $total_venta = $cart->total();
+
+        $tipoPago_id = $this->request->getPost('tipoPago_id');
+        $tarjeta = $this->request->getPost('tarjeta');
+        $direccion = $this->request->getPost('direccion');
+        $ciudad = $this->request->getPost('ciudad');
+        $provincia = $this->request->getPost('provincia');
+        $codigo_postal = $this->request->getPost('codigo_postal');
+        $metodo_envio = $this->request->getPost('metodo_envio');
+        $costo_envio = $this->request->getPost('costo_envio');
+
+        $ventasCabecera = new VentasCabecera();
+        $ventasCabeceraData = [
+            'fecha' => date('Y-m-d H:i:s'),
+            'usuario_id' => $usuario_id,
+            'total_venta' => $total_venta,
+            'tipoPago_id' => $tipoPago_id,
+            'tarjeta' => $tarjeta,
+        ];
+
+        if (!empty($ventasCabeceraData)) {
+            $ventasCabecera->insert($ventasCabeceraData);
+            $venta_id = $ventasCabecera->insertID();
+        }
+
+        $ventasDetalle = new VentasDetalle();
+        foreach ($carrito as $producto) {
+            $ventasDetalleData = [
+                'venta_id' => $venta_id,
+                'producto_id' => $producto['id'],
+                'cantidad' => $producto['qty'],
+                'precio' => $producto['price'],
+            ];
+            $ventasDetalle->insert($ventasDetalleData);
+            $producto_id = $producto['id'];
+            $cantidad_actual = $productoModelo->getCantidad($producto_id);
+
+            $nueva_cantidad = $cantidad_actual - $producto['qty'];
+
+            $productoModelo->updateCantidad($producto_id, $nueva_cantidad);
+        }
+        //Guardar los datos de envio
+        $enivo = new EnvioDetalle();
+        $envioData = [
+            'venta_id' => $venta_id,
+            'direccion' => $direccion,
+            'ciudad' => $ciudad,
+            'provincia' => $provincia,
+            'codigo_postal' => $codigo_postal,
+            'metodo_envio' => $metodo_envio,
+            'costo_envio' => 3500,
+            'fecha_envio' => date('Y-m-d H:i:s'),
+        ];
+        $enivo->insert($envioData);
+
+        $cart->destroy();
+
+        // Redirigir a una página de éxito
+        return redirect()->to('ventas/success')->with('mensaje', 'La compra se ha realizado con éxito.');
+    }
+
+    public function success()
+    {
+        echo view('components/header');
+        echo view('Products/success');
+        echo view('components/footer');
     }
 }
